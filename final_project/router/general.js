81a2989b7e8470ad412e24d6e5e371e21ff79d4a
DevOpsCraftsman <alexandre.poitevin.dev@gmail.com>
2024-05-17 16:05:28 +0100

    Refactor to Promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,35 +13,50 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/',async function (req, res) {
   //Write your code here
-  res.send(present(books))
+  const allBooks = await new Promise((resolve, reject) => {
+    setTimeout(() => resolve(books))
+  })
+  res.send(present(allBooks))
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   //Write your code here
-  res.send(present(books[req.params.isbn]))
+  const bookByIsbn = await new Promise((resolve, reject) => {
+    setTimeout(() => resolve(books[req.params.isbn]))
+  })
+  res.send(present(bookByIsbn))
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   //Write your code here
-  const booksbyauthor = Object.values(books).filter(b => b.author === req.params.author)
-  res.send(present({ booksbyauthor }))
+  const booksByAuthor = await new Promise((resolve, reject) => {
+    setTimeout(() => resolve(
+      Object.values(books).filter(b => b.author === req.params.author)
+    ))
+  })
+  res.send(present(booksByAuthor))
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   //Write your code here
-  const booksbytitle = Object.values(books).filter(b => b.title == req.params.title)
-  res.send(present({ booksbytitle }))
+  const booksByTitle = await new Promise((resolve, reject) => {
+    setTimeout(() => resolve(
+      Object.values(books).filter(b => b.title == req.params.title)
+    ))
+  })
+  res.send(present(booksByTitle))
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  res.send(present(books[req.params.isbn].reviews))
+  const reviews = books[req.params.isbn].reviews
+  res.send(present(reviews))
 });
 
 module.exports.general = public_users;
